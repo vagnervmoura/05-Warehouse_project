8034e6bcab41a75d00a8be6a90827b7269c8cd49
Vagner Vignoto Moura <eu@vagnermoura.com>
2023-09-18 06:54:53 +0100

    Function BALANCE done.

--- main.py
+++ main.py
@@ -3,13 +3,33 @@
 # The program should handle various commands for performing operations like:
 # adding/subtracting balance, recording sales and purchases, displaying account balance, showing warehouse status, and reviewing recorded operations.
 option = None
+balance = 0
 
 def goto(linenum):
     global line
     line = linenum
 
+def balance_1(): # The program will prompt for an amount to add or subtract from the account.
+    global balance
+    try:
+        action = int(input("Press '1' to Add or press '2' to Subtract: "))
+        print(action == 1)
+        if action == 1:
+            value = float(input("1 - Insert the value to add to your balance: "))
+            balance += value
+            print("Your new balance is: {}".format(balance))
+        elif action == 2:
+            value = float(input("2 - Insert the value to subtract from your balance:"))
+            if value < balance:
+                balance -= value
+                print("Your new balance is: {}".format(balance))
+            else:
+                print("Sorry, you do not have balance enough to do this withdraw.")
+    except ValueError:
+        print("Sorry.\n")
+    
 
-while True:
+while option != 0:
     option = int(input("\n\nSelect one of the following options:\n1 - balance\n2 - sale\n3 - purchase\n4 - account\n5 - list\n6 - warehouse\n7 - review\n0 - end\n\n"))
 
     try:
@@ -19,6 +39,7 @@ while True:
             
         elif option == 1: # Option to balance
             print("You choose the Option {} to check the Balance.".format(option))
+            balance_1()
             
             
         elif option == 2: # Option to sale
